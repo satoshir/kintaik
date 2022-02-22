401f44b12a7ee5e30c76bc37694a1cac140c476f
satoshi@email.com <ec2-user@ip-172-31-5-128.ap-northeast-1.compute.internal>
2022-02-22 13:24:19 +0000

    6.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+  
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def cuttent_user
+    if session[:user_id]
+       @cuttent_user ||=User.find_by(id: session[:user_id])
+    end
+  end
 end
